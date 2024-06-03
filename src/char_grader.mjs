dae4bc86d4c849920a62eb7998fe285ccd9f45a1
steve02081504 <steve02081504@EDEN.fukwold>
2024-06-04 01:02:51 +0800

    update

--- src/char_grader.mjs
+++ src/char_grader.mjs
@@ -2,6 +2,7 @@ import { read } from './character-card-parser.mjs';
 import { Tiktoken } from "js-tiktoken/lite";
 import { o200k_base } from './o200k_base.mjs';
 import { arraysEqual } from './tools.mjs';
+import { compressToUTF16 } from 'lz-string';
 function simple_marco_remover(str) {
 	return str.replace(/{{\/\/([\s\S]*?)}}/g, '').replace(/\{\{(user|char)\}\}/gi, 'name');
 }
@@ -28,6 +29,8 @@ export function char_grader(arg, progress_stream = console.log) {
 	let json = arg
 	var score_details = {
 		name: json.name,
+		tags: json?.tags || [],
+		index: json?.creatorcomment,
 		logs: [],
 		score: 0
 	}
@@ -91,26 +94,15 @@ export function char_grader(arg, progress_stream = console.log) {
 			do_reparation(title, size, scale, reparation_scale)
 		return size
 	}
-	if (cardsize) {
-		let cardsizeMB = cardsize / 1024 / 1024
-		score_details.score += cardsizeMB
-		score_details.logs.push({
-			type: 'card size',
-			score: cardsizeMB
-		})
-		progress_stream(`card size: ${cardsize} bytes, ${cardsizeMB} scores.`)
-		if (cardsizeMB > 100)
-			do_reparation('card size', cardsizeMB)
-	}
 	GradingByTokenSize('description & constant WI infos & mes_example', [
-		format_text, json.mes_example,
+		format_text, json.mes_example
 	], 1, 9037)
 	GradingByTokenSize('personality & scenario', [
 		json.personality, json.scenario
 	], 0.5)
 	let charData = json.data
 	GradingByTokenSize('system_prompt & depth_prompt', [
-		charData.system_prompt, charData?.extensions?.depth_prompt?.prompt
+		charData?.system_prompt, charData?.extensions?.depth_prompt?.prompt
 	], 0.3)
 	if (format_text.includes('    ') || format_text.includes('\t')) {
 		let format_name = ''
@@ -181,6 +173,77 @@ export function char_grader(arg, progress_stream = console.log) {
 			score: charData.extensions.group_greetings.length * 20
 		})
 		progress_stream(`group_greetings: ${charData.extensions.group_greetings.length} group greetings, ${charData.extensions.group_greetings.length * 20} scores.`)
+	}
+	// 通过gzip压缩人物数据来得知数据冗余度，比较压缩率来同步缩放分数
+	let gzip_text = [
+		json.description, json.mes_example,
+		json.personality, json.scenario,
+		charData?.system_prompt, charData?.extensions?.depth_prompt?.prompt,
+		...(json?.data?.character_book?.entries?.map?.(_ => _.content) || []),
+		...(charData?.alternate_greetings || []),
+		...(charData?.extensions?.group_greetings || []),
+	].filter(_ => _?.length).join('\n')
+	let compressed = compressToUTF16(gzip_text)
+	let compress_ratio = compressed.length / gzip_text.length
+	score_details.score *= compress_ratio
+	score_details.logs.push({
+		type: 'compress_ratio',
+		scale: compress_ratio
+	})
+	progress_stream(`compress_ratio: ${compress_ratio}, all scores scaled as ${compress_ratio}.`)
+
+	if (cardsize) {
+		let cardsizeMB = cardsize / 1024 / 1024
+		score_details.score += cardsizeMB
+		score_details.logs.push({
+			type: 'card size',
+			score: cardsizeMB
+		})
+		progress_stream(`card size: ${cardsize} bytes, ${cardsizeMB} scores.`)
+		if (cardsizeMB > 100)
+			do_reparation('card size', cardsizeMB)
+	}
+	json.creatorcomment = json.creatorcomment || ""
+	let cleard_creatorcomment = json.creatorcomment.split('\n').filter(
+		_ => !(
+			_.includes('http') || _.includes('Discord') || _.includes('GitHub') ||
+			_.includes('类脑') || _.includes('Telegram') || _.includes('社区')
+		) && _.trim().length
+	).join('\n')
+	if (cleard_creatorcomment) {
+		let score = 5 + json.creatorcomment.length / 125
+		score_details.score += score
+		score_details.logs.push({
+			type: 'creatorcomment',
+			score: score
+		})
+		progress_stream(`creatorcomment: ${json.creatorcomment.length} bytes, ${score} scores.`)
+	}
+	else {
+		let diff = -50
+		score_details.score += diff
+		score_details.logs.push({
+			type: 'creatorcomment not found',
+			score: diff
+		})
+		progress_stream(`creatorcomment not found: ${diff} scores.`)
+	}
+	if (json?.tags?.length) {
+		score_details.score += json.tags.length * 3
+		score_details.logs.push({
+			type: 'tags',
+			score: json.tags.length * 3
+		})
+		progress_stream(`tags: ${json.tags.length} tags, ${json.tags.length * 3} scores.`)
+	}
+	else {
+		let diff = -5
+		score_details.score += diff
+		score_details.logs.push({
+			type: 'tags not found',
+			score: diff
+		})
+		progress_stream(`tags not found: ${diff} scores.`)
 	}
 	return score_details
 }
